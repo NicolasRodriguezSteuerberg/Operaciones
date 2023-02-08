f289509e293a9d98a7df4079fee7595d6ef097ef
Nicolas Rodriguez Steuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-02-08 14:08:00 +0100

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int opcion;
+        do{
+            float resultado;
+            opcion = EntradaSalida.entradaInt("1 --> Hacer suma\n2 --> Hacer resta\n3 --> Hacer multiplicación\n4 --> Hacer división\n5 --> Saír do programa");
+            if(opcion > 0 && opcion < 5) {
+                resultado = OperacionesBasicas.realizar(opcion, EntradaSalida.entradaFloat("Teclee o primer número da operación"), EntradaSalida.entradaFloat("Teclee o segundo número da operación"));
+                EntradaSalida.salida(("O resultado da operación é " + resultado), EntradaSalida.SALIDA_WINDOW);
+            }
+        }while (opcion > 0 && opcion < 5);
+        EntradaSalida.salida("Saíndo do programa", EntradaSalida.SALIDA_WINDOW);
+    }
+}
---
+++ src/OperacionesBasicas.java
@@ -0,0 +1,57 @@
+public class OperacionesBasicas {
+    private static float resultado;
+    static final int SUMA = 1;
+    static final int RESTA = 2;
+    static final int MULTIPLICACION = 3;
+    static final int DIVISION = 4;
+
+    /**
+     * @param device --> operación a realizar (SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN)
+     * @param num1 --> primer numero de la operación
+     * @param num2 --> segundo numero de la operación (divisor en caso de division)
+     * @return resultado
+     */
+    public static float realizar(int device, float num1, float num2){
+        switch (device){
+            case SUMA:
+                try{
+                    resultado = num1 + num2;
+                }catch (Exception e){
+                    System.out.println("Operación no válida");
+                }
+                break;
+            case RESTA:
+                try{
+                    resultado = num1 - num2;
+                }catch (Exception e){
+                    System.out.println("Operación no válida");
+                }
+                break;
+            case MULTIPLICACION:
+                try{
+                    resultado = num1*num2;
+                }catch (Exception e){
+                    System.out.println("Operación no válida");
+                }
+                break;
+            case DIVISION:
+                try{
+                    resultado = num1/num2;
+                }catch (Exception e){
+                    System.out.println("Operación no válida");
+                }
+                break;
+        }
+        return resultado;
+    }
+
+
+    //SETTERS Y GETTERS
+    public void setResultado(float resultado) {
+        this.resultado = resultado;
+    }
+
+    public float getResultado() {
+        return resultado;
+    }
+}
