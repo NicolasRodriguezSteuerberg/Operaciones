8abe521fa8ce049f0fab82f6cc3297780358794c
Nicolas Rodriguez Steuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-02-10 09:46:05 +0100

    final

--- src/OperacionesBasicas.java
+++ src/OperacionesBasicas.java
@@ -1,5 +1,5 @@
 public class OperacionesBasicas {
-    private static float resultado;
+    private static Float resultado = null;
     static final int SUMA = 1;
     static final int RESTA = 2;
     static final int MULTIPLICACION = 3;
@@ -12,33 +12,34 @@ public class OperacionesBasicas {
      * @return resultado
      */
     public static Float realizar(int device, float num1, float num2){
+
         switch (device){
             case SUMA:
                 try{
                     resultado = num1 + num2;
                 }catch (Exception e){
-                    System.out.println("Operación no válida");
+                    System.out.println("Error");
                 }
                 break;
             case RESTA:
                 try{
                     resultado = num1 - num2;
                 }catch (Exception e){
-                    System.out.println("Operación no válida");
+                    System.out.println("Error");
                 }
                 break;
             case MULTIPLICACION:
                 try{
                     resultado = num1*num2;
                 }catch (Exception e){
-                    System.out.println("Operación no válida");
+                    System.out.println("Error");
                 }
                 break;
             case DIVISION:
                 try{
                     resultado = num1/num2;
                 }catch (Exception e){
-                    System.out.println("Operación no válida");
+                    System.out.println("Error");
                 }
                 break;
         }
