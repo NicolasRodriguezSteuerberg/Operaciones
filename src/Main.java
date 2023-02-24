6b7d81b2db81a55b81d9c12e0ddb4085e7e2036b
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-02-24 09:04:08 +0100

    Merge SQUASH

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,28 @@ public class Main {
         int opcion;
         Float numero1, numero2;
         Float resultado;
-        opcion = EntradaSalida.entradaInt("1 --> Hacer suma\n2 --> Hacer resta\n3 --> Hacer multiplicación\n4 --> Hacer división\n5 --> Saír do programa");
+        opcion = EntradaSalida.entradaInt("1 --> Hacer suma\n2 --> Hacer resta\n3 --> Hacer multiplicación\n4 --> Hacer división\n5 --> Raiz con cualquier indice\n6 --> Saír do programa");
         numero1 = EntradaSalida.entradaFloat("Teclee o primer número da operación");
-        numero2 = EntradaSalida.entradaFloat("Teclee o segundo número da operación");
-        if(numero1 == null || numero2==null){
-            EntradaSalida.salida("Tiene que ser un número, no una letra", EntradaSalida.SALIDA_CONSOLA);
+        if(numero1 == null){
+            EntradaSalida.salida("El primer numero tiene que ser un número válido", EntradaSalida.SALIDA_CONSOLA);
         }
         else {
-            if (opcion > 0 && opcion < 5) {
-                resultado = OperacionesBasicas.realizar(opcion, numero1, numero2);
-                if (Float.isNaN(resultado)) {
-                    EntradaSalida.salida("Error", EntradaSalida.SALIDA_CONSOLA);
-                } else {
-                    EntradaSalida.salida(("O resultado da operación é " + resultado), EntradaSalida.SALIDA_WINDOW);
+            numero2 = EntradaSalida.entradaFloat("Teclee o segundo número da operación");
+            if(numero2==null){
+                EntradaSalida.salida("Tiene que ser un número, no una letra", EntradaSalida.SALIDA_CONSOLA);
+            }
+            else {
+                if (opcion > 0 && opcion < 6) {
+                    resultado = OperacionesBasicas.realizar(opcion, numero1, numero2);
+                    if (Float.isNaN(resultado)) {
+                        EntradaSalida.salida("Error", EntradaSalida.SALIDA_CONSOLA);
+                    }
+                    else {
+                        EntradaSalida.salida(("O resultado da operación é " + resultado), EntradaSalida.SALIDA_WINDOW);
+                    }
+                }
+                else {
+                    EntradaSalida.salida("A opción foi incorrecta, saindo", EntradaSalida.SALIDA_WINDOW);
                 }
             }
         }
--- src/OperacionesBasicas.java
+++ src/OperacionesBasicas.java
@@ -4,11 +4,12 @@ public class OperacionesBasicas {
     static final int RESTA = 2;
     static final int MULTIPLICACION = 3;
     static final int DIVISION = 4;
+    static final int RAICES = 5;
 
     /**
-     * @param device --> operación a realizar (SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN)
-     * @param num1 --> primer numero de la operación
-     * @param num2 --> segundo numero de la operación (divisor en caso de division)
+     * @param device --> operación a realizar (SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN, RAICES)
+     * @param num1 --> primer numero de la operación (en caso de raiz es el indice)
+     * @param num2 --> segundo numero de la operación (divisor en caso de division, en caso de raiz el radicando)
      * @return resultado
      */
     public static Float realizar(int device, float num1, float num2){
@@ -42,6 +43,12 @@ public class OperacionesBasicas {
                     System.out.println("Error");
                 }
                 break;
+            case RAICES:
+                try {
+                    resultado = (float) Math.pow(1 / num1, num2);
+                }catch (Exception e){
+                    System.out.println("Error");
+                }
         }
         return resultado;
     }
